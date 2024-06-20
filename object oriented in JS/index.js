1f2b691fd9f1270df26133be8de8329db7c24941
Kapil Gupta <kapil.gupta4949@gmail.com>
2024-06-21 01:00:20 +0530

    prototypal inheritance half way done

--- object oriented in JS/index.js
+++ object oriented in JS/index.js
@@ -1,58 +1,144 @@
-// 1.
-function User(name, age) {
-    this.name = name
-    this.age = age
+/*
+  OOPS -> Object Oriented Programming.
 
-    return this  // we dont need to return "this" if we are using "new" keyword.
-}
+  In OOPS language prime focus is Object (creation, manipulation, destruction)
 
-const user = User("kapil", 28);
-console.log(user);
+  In scripting language (JS) the prime focus is Functions. 
 
-const user2 = User("shubham", 26);
+  JS is a FUNCTIONAL Language.
+  JS has LEss support of OOPS.
 
-const user3 = new User("aniket", 28);
-console.log(user3);
 
-const user4 = new User("anirudh", 28);
-console.log(user4);
+  // Main thing is OOPS and FUNCTIONAL are paradigms (WAY OF LIFE)
 
-const user5 = new User("shreyas", 28);
-console.log(user5.constructor);
+  // Main aim of any program (software) is to create Objects and handle them.
+  // Objects can be created through OOPS and FUNCTIONAL programming.
 
-// 2. Implementing inheritance using constructor function.
-function Animal(name) {
-    console.log("this in animal:", this);
+  // OOPS: 
+
+  1. Object 
+  2. Class 
+  3. Inheritance
+  4. Polymorphism 
+  5. Abstraction 
+  6. Encapsulation 
+*/
+
+// --------------- Different ways to create object in JavaScript -------------------
+
+// WAY-1: Using simple object literal syntax.
+
+const person1 = {
+  name: "Person 1",
+  age: 25,
+  // Methods
+  canWalk: function () {
+    console.log("hello I can walk " + this.name);
+  },
+};
+
+console.log("person 1:", person1);
+
+const person2 = {
+  name: "Person 2",
+  age: 27,
+  // Methods
+  canWalk: function () {
+    console.log("hello I can walk " + this.name);
+  },
+};
+
+console.log("person 2:", person2);
+
+// WAY-2: Using class syntax.
+// NOTE: Classes in JS are just syntactical sugar. Behind the scenes JS uses constructor functions only.
+
+class Person {
+  // OPTIONAL HERE
+  name;
+  age;
+
+  constructor(name, age) {
     this.name = name;
+    this.age = age;
+    this.country = "India";
+  }
+
+  // METHODS
+  canWalk() {
+    console.log("hello I can walk" + this.name);
+  }
 }
 
-// Animal.prototype.sayName = function() {
-//     console.log("My name is " + this.name);
-// };
+const person3 = new Person("Person 3", 27);
+console.log("person 3:", person3);
 
-console.log("animal proto:", Animal.prototype);
+const person4 = new Person("Person 4", 21);
+console.log("person 4:", person4);
 
-animal = new Animal("anmia1")
-console.log("animal:", animal);
+const person5 = new Person("Person 5", 32);
+console.log("person 5:", person5);
 
-function Dog(name, breed) {
-    console.log("this in dog:", this);
-    Animal.call(this, name); // Call the parent constructor with the current context -> {}
-    this.breed = breed;
+const person6 = new Person("Person 6", 24);
+console.log("person 6:", person6);
+
+/*
+    Comparing person1 (Normal object) vs person3 (class object).
+    Only difference is the method canWalk() otherwise EVERYTHING else is same.
+  
+    canWAlk method -> OWN method/property in person1.
+    canWAlk method -> NOT OWN method/property in person3.
+*/
+
+// WAY-3: Constructor functions.
+// NOTE: Before "class" keyword i.e. (BEFORE ES6) we had constructor functions.
+
+function PersonFunction(name, age) {
+  this.name = name;
+  this.age = age;
+  this.country = "Mumbai";
+
+  this.canWalk = function () {
+    console.log("hello I can walk " + this.name);
+  };
 }
 
-// Dog.prototype = Object.create(Animal.prototype); // Set Dog's prototype to an instance of Animal.
-// Dog.prototype.constructor = Dog; // Set the constructor property back to Dog.
+const person7 = new PersonFunction("Person 7", 29);
+console.log("person 7:", person7);
+const person8 = new PersonFunction("Person 8", 22);
+console.log("person 8:", person8);
 
-// console.log("dog proto before:", Dog.prototype);
+person5.canWalk();
 
-// Dog.prototype.bark = function() {
-//     console.log("Woof!");
-// };
+/*
+  Having look at Constructor Function and Classes.
+  we can conclude EVERYTHING is same except the methods.
 
-console.log("dog proto after:", Dog.prototype);
+  Methods in constructor function -> Own methods. 
+  Methods in class -> Not own methods.
+*/
 
-const myDog = new Dog("Max", "Labrador");
-console.log("dog:", myDog);
-// myDog.sayName(); // Output: My name is Max
-// myDog.bark();    // Output: Woof!
+// Now here is the question: Which approach of object creation is better?
+// Constructor Function Vs Class Vs Normal Objects?
+
+// Answer:
+
+// Check for the constructor function methods.
+const constructorFunctionCanWalkResult = person7.canWalk === person8.canWalk;
+console.log(
+  "constructorFunctionCanWalkResult:",
+  constructorFunctionCanWalkResult
+); // false
+
+// Check for the class methods.
+const classCanWalkResult1 = person3.canWalk === person4.canWalk;
+const classCanWalkResult2 = person5.canWalk === person6.canWalk;
+
+console.log("classCanWalkResults", classCanWalkResult1, classCanWalkResult2); // true true
+
+/*
+  In "constructor function" if you create 10000 object then we will have 10000 copies of "canWalk" method also.
+  But in "class" if you create 10000 object then we will have 1 copy of "canWalk" method only. 
+
+  Thats IT, CLASSES WON !!!!
+*/
